5230b74636299947f4dd18410ba0e87239f08a12
nguy_k <kevin1.nguyen@epitech.eu>
2015-09-29 23:24:17 +0200

    Init angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTryApp
+ * @description
+ * # angularTryApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTryApp', []);
